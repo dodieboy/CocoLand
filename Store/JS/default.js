042589a8d51760f544b8dd4acbe2d57d3fc79bae
Alan Tan <dodieboy@users.noreply.github.com>
2023-04-22 15:59:13 +0800

    Updated login_check()
    - no longer use using loginCheck.php
    - no longer auto redirect user to store, login or admin

--- Store/JS/default.js
+++ Store/JS/default.js
@@ -4,7 +4,7 @@ SPDX-Short-Identifier: MIT
 This code is licensed under MIT license (See LICENSE.txt for details)
 */
 $(document).ready(function() {
-    //login_check();
+    login_check();
     if ($(window).width() < 777) {
         cart_check('mobile');
     } else {
@@ -28,35 +28,18 @@ $(window).resize(function() {
 })
 
 function login_check() {
-    $.ajax({
-        type: "POST",
-        url: 'loginCheck.php',
-        dataType: 'json',
-        success: function(data) {
-            if (data[0].indexOf('fail') >= 0) {
-                $('#navLogin').html('<a href="login.html">Login Now</a>');
-                if (($(location).attr('pathname').indexOf('admin.html') >= 0) || ($(location).attr('pathname').indexOf('addUser.html') >= 0) || ($(location).attr('pathname').indexOf('edit.html') >= 0)) {
-                    window.location = 'index.html';
-                }
-            } else {
-                $('#navLogin').html('<a href="logout.php">Logout Now</a>');
-                if (($(location).attr('pathname').indexOf('register.html') >= 0) || ($(location).attr('pathname').indexOf('login.html') >= 0)) {
-                    window.location = 'index.html';
-                }
-                if (data[1].indexOf('N') >= 0) {
-                    $('.UName').html('Welcome ' + data[0]);
-                    if (($(location).attr('pathname').indexOf('admin.html') >= 0) || ($(location).attr('pathname').indexOf('addUser.html') >= 0) || ($(location).attr('pathname').indexOf('edit.html') >= 0)) {
-                        window.location = 'index.html';
-                    }
-                } else if (data[1].indexOf('A') >= 0) {
-                    $('.UName').html('Welcome ' + '<b><a href="admin.html" class="user" title="Go admin portal">' + data[0] + '</a></b>');
-                }
-            }
-        },
-        error: function(data) {
-            alert("error: Login failed");
-        }
-    })
+    if(Cookies.get('login')){
+        return true;
+    }
+    return false;
+}
+
+function nav_login() {
+    if (login_check()) {
+        $('#navLogin').html('<a href="logout.php">Logout Now</a>');
+        return;
+    }
+    $('#navLogin').html('<a href="login.html">Login Now</a>');
 }
 
 function cart_check(i) {
