8649fb6643093a120ee281bc755500ca39a125ef
Alan Tan <dodieboy@users.noreply.github.com>
2023-04-24 17:06:28 +0800

    Update login code
    -no longer use using login.php & loginCheck.php and are removed

--- Store/JS/login.js
+++ Store/JS/login.js
@@ -6,20 +6,26 @@ This code is licensed under MIT license (See LICENSE.txt for details)
 $(document).ready(function() {
     $("#login").on("submit", function(e) {
         e.preventDefault();
-        var tempPass = $.md5($('#password').val());
-        $.ajax({
-            type: "POST",
-            url: 'login.php',
-            data: $(this).serialize() + "&md5Password=" + tempPass,
-            success: function(data) {
-                $('#result').html(data);
-                $('#result').show();
-                if (data.indexOf('Success') >= 0) {
-                    roleCheck();
-                } else {
-                    $('#password').val("");
+        var hashPass = $.md5($('#password').val());
+        $.getJSON("json/user.json", {}, function(data) {
+            var data = data.user;
+            console.log($('#username').val());
+            console.log(hashPass);
+            if (data.length !== 0) {
+                for (let i = 0; i < data.length; i++) {
+                    if (data[i].username === $('#username').val() && data[i].password === hashPass) {
+                        console.log(1);
+                        Cookies.set('Name', data[i].name, { sameSite: 'strict' });
+                        setToken(data[i].id);
+                        roleCheck();
+                        return;
+                    }
                 }
             }
+            $('#result').html("Error: Username or password is wrong");
+            $('#result').show();
+        }).fail(function () {
+            console.log("Error: failed to connect to JWT server!");
         })
     })
     $("#register").on("submit", function(e) {
@@ -42,16 +48,40 @@ $(document).ready(function() {
 })
 
 function roleCheck() {
-    $.ajax({
-        type: "POST",
-        url: 'loginCheck.php',
-        dataType: 'json',
-        success: function(data) {
-            if (data[1] == "A") {
-                window.location = 'admin.html';
-            } else {
-                window.location = 'index.html';
+    var result = false;
+    $.getJSON("json/session.json", {}, function(data) {
+        var data = data.session;
+        if (data.length !== 0) {
+            for (let i = 0; i < data.length; i++) {
+                if (data[i].token === Cookies.get("JWT")) {
+                    if (data[i].role === 'N') {
+                        window.location = 'index.html';
+                    } else if (data[i].role === 'A') {
+                        window.location = 'admin.html';
+                    }
+                    result = true;
+                    return;
+                }
             }
         }
+    }).fail(function () {
+        console.log("Error: failed to connect to JWT server!");
+    })
+    return result;
+}
+
+function setToken(id){
+    $.getJSON("json/session.json", {}, function(data) {
+        var data = data.session;
+        if (data.length !== 0) {
+            for (let i = 0; i < data.length; i++) {
+                if (data[i].user_id === id) {
+                    Cookies.set('JWT', data[i].token, { sameSite: 'strict' });
+                    return;
+                }
+            }
+        }
+    }).fail(function () {
+        console.log("Error: failed to connect to JWT server!");
     })
 }
